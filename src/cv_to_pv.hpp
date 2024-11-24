9b3cace21b62e37db2197234e3c3d8b0364a8e27
Christian Rauch <Rauch.Christian@gmx.de>
2024-11-24 12:23:05 +0100

    Imported upstream version '0.2.0' of 'upstream'

--- src/cv_to_pv.hpp
+++ src/cv_to_pv.hpp
@@ -1,11 +1,24 @@
 #pragma once
-#include <cstddef>
-#include <libcamera/controls.h>
 #include <rclcpp/parameter_value.hpp>
+#include <stdexcept>
+#include <string>
+
+namespace libcamera
+{
+class ControlId;
+class ControlValue;
+} // namespace libcamera
+
+
+class invalid_conversion : public std::runtime_error
+{
+public:
+  explicit invalid_conversion(const std::string &msg) : std::runtime_error(msg) {}
+};
 
 
 rclcpp::ParameterValue
-cv_to_pv(const libcamera::ControlValue &value, const std::size_t &extent);
+cv_to_pv(const libcamera::ControlValue &value);
 
 rclcpp::ParameterType
-cv_to_pv_type(const libcamera::ControlType &type, const bool is_array);
+cv_to_pv_type(const libcamera::ControlId *const id);
