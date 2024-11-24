640bba09ca34503fe30a8cf58b771b92d86c00cd
Christian Rauch <Rauch.Christian@gmx.de>
2024-11-24 12:23:18 +0100

    Rebase from 'release/humble/camera_ros'

--- src/format_mapping.cpp
+++ src/format_mapping.cpp
@@ -1,19 +1,26 @@
 #include "format_mapping.hpp"
 #include <cstdint>
 #include <libcamera/formats.h>
+#include <libcamera/geometry.h>
 #include <libcamera/pixel_format.h>
+#include <libcamera/stream.h>
+#include <map>
 #include <sensor_msgs/image_encodings.hpp>
 #include <unordered_map>
+#include <utility>
+#include <vector>
 
 
 namespace cam = libcamera::formats;
 namespace ros = sensor_msgs::image_encodings;
 
-//mapping of FourCC to ROS image encodings
-// see 'include/uapi/drm/drm_fourcc.h' for a full FourCC list
+// mapping of FourCC to ROS image encodings
+// references:
+// - full list of FourCC: 'include/uapi/drm/drm_fourcc.h'
+// - V4L2 image formats: 'https://docs.kernel.org/userspace-api/media/v4l/pixfmt.html'
 
 // supported FourCC formats, without conversion
-const std::unordered_map<uint32_t, std::string> map_format_raw = {
+static const std::unordered_map<uint32_t, std::string> map_format_raw = {
   // RGB encodings
   // NOTE: Following the DRM definition, RGB formats codes are stored in little-endian order.
   {cam::R8.fourcc(), ros::MONO8},
@@ -26,6 +33,8 @@ const std::unordered_map<uint32_t, std::string> map_format_raw = {
   // YUV encodings
   {cam::YUYV.fourcc(), ros::YUV422_YUY2},
   {cam::UYVY.fourcc(), ros::YUV422},
+  {cam::NV21.fourcc(), ros::NV21},
+  {cam::NV24.fourcc(), ros::NV24},
   // Bayer encodings
   {cam::SRGGB8.fourcc(), ros::BAYER_RGGB8},
   {cam::SGRBG8.fourcc(), ros::BAYER_GRBG8},
@@ -38,7 +47,7 @@ const std::unordered_map<uint32_t, std::string> map_format_raw = {
 };
 
 // supported FourCC formats, without conversion, compressed
-const std::unordered_map<uint32_t, std::string> map_format_compressed = {
+static const std::unordered_map<uint32_t, std::string> map_format_compressed = {
   {cam::MJPEG.fourcc(), "jpeg"},
 };
 
@@ -61,4 +70,17 @@ format_type(const libcamera::PixelFormat &pixelformat)
   if (map_format_compressed.count(pixelformat.fourcc()))
     return FormatType::COMPRESSED;
   return FormatType::NONE;
+}
+
+libcamera::StreamFormats
+get_common_stream_formats(const libcamera::StreamFormats &formats)
+{
+  std::map<libcamera::PixelFormat, std::vector<libcamera::SizeRange>> common_stream_formats;
+  for (const libcamera::PixelFormat &fmt : formats.pixelformats()) {
+    if (format_type(fmt) != FormatType::NONE) {
+      common_stream_formats[fmt] = {formats.range(fmt)};
+    }
+  }
+
+  return {common_stream_formats};
 }
