fd73bf7d1127ffab35f58609302a75292395cd55
Christian Rauch <Rauch.Christian@gmx.de>
2025-01-02 14:10:14 +0100

    Rebase from 'release/jazzy/camera_ros'

--- src/pretty_print.cpp
+++ src/pretty_print.cpp
@@ -9,6 +9,7 @@
 #include <libcamera/stream.h>
 #include <memory>
 #include <optional>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -36,20 +37,21 @@ operator<<(std::ostream &out, const libcamera::StreamFormats &formats)
       << ">> stream formats:";
   for (const libcamera::PixelFormat &pixelformat : formats.pixelformats()) {
     out << std::endl
-        << "   - Pixelformat: " << pixelformat.toString() << " ("
+        << "   - " << pixelformat.toString() << " ("
         << formats.range(pixelformat).min.toString() << " - "
         << formats.range(pixelformat).max.toString() << ")";
   }
   return out;
 }
 
-std::ostream &
-operator<<(std::ostream &out, const libcamera::StreamConfiguration &configuration)
+std::string
+list_format_sizes(const libcamera::StreamConfiguration &configuration)
 {
+  std::ostringstream out;
   out << std::endl
       << ">> " << configuration.pixelFormat << " format sizes:";
   for (const libcamera::Size &size : configuration.formats().sizes(configuration.pixelFormat))
     out << std::endl
         << "   - " << size.toString();
-  return out;
+  return out.str();
 }
